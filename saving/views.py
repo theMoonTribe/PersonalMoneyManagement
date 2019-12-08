ee0b19c29fa7c4b1c05372770a2bacd322685e2b
wongyatho <wongyatho2@gmail.com>
2019-12-08 21:11:44 +0800

    Added Django Template to standardize navbar and using bulma

--- saving/views.py
+++ saving/views.py
@@ -4,10 +4,10 @@ Created on 8 Dec 2019
 @author: raywong
 '''
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'index.html')
 
 def home(request):
     return HttpResponse("Home!")
